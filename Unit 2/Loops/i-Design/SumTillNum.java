8616a12319ca0d9dffc9bfbe5b31b4a4cdc4479b
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-02-15 15:45:56 +0530

    Create SumTillNum.java

---
+++ Unit 2/Loops/i-Design/SumTillNum.java
@@ -0,0 +1,27 @@
+/*
+  Write a program to read in numbers until the number -999 is encountered. The sum of all numbers read until this point should be printed out. (exclude -999 when calculating the sum)
+*/
+import java.util.*;
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        //Fill your code here
+        Scanner scn = new Scanner(System.in);
+
+        int sum = 0;
+        
+        while (true) {
+            int n = scn.nextInt();
+            if(n == -999)
+                break;
+            else
+                sum = sum + n; 
+            
+        }
+        scn.close();
+
+        System.out.println("The sum is " + sum);
+    }
+}
