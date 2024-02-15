399680e75e542591d7b78626df32df74f0250d77
Rajan Dhiman <dhimanrajan7252@gmail.com>
2024-02-15 18:39:47 +0530

    Create question4.java

---
+++ Unit 2/Loops/i-Design/question4.java
@@ -0,0 +1,30 @@
+
+/*
+  ****
+  ****
+  ****
+  ****
+*/
+import java.util.*;
+
+class Demo
+{
+    public static void main(String args[])
+    {
+        //Fill your code here
+        // Scanner scn = new Scanner(System.in);
+
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        scn.close();
+
+        for(int i= 1 ; i<=n; i++)
+        {
+            for(int j = 1 ;  j<=n;j++)
+            {
+                System.out.print('*');
+            }
+            System.out.println();
+        }
+    }
+}
